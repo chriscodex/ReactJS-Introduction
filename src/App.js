ea403d22741045fc28ad7d47973c472f186c5cb8
Christian <c.espinoza.code@gmail.com>
2023-03-17 21:40:32 -0500

    app.css ignored

--- src/App.js
+++ src/App.js
@@ -4,3 +4,4 @@ import { TodoSearch } from './TodoSearch.js';
 import { TodoList } from './TodoList.js';
 import { TodoItem } from './TodoItem.js';
 import { CreateTodoButtom } from './CreateTodoButton.js';
+//import './App.css';
