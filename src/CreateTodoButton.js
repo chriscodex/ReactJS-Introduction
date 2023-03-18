ef03a28f6f34aab05fb377363d0a2800e1f726bf
Christian <c.espinoza.code@gmail.com>
2023-03-18 08:46:13 -0500

    react imported to createTodoButton component

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,17 +1,1 @@
 import React from "react";
-import "./CreateTodoButton.css"
-
-function CreateTodoButtom(props){
-    const onclickButton = () => {
-        alert('Alerta de Modal')
-    }
-
-    return(
-        <button 
-        className="CreateTodoButton"
-        onClick={onclickButton}
-        >+</button>
-    );
-}
-
-export { CreateTodoButtom};
