53293bffc57839284af839e1b8cc96a28993c3b4
Christian <c.espinoza.code@gmail.com>
2023-03-17 21:39:50 -0500

    TodoItem component imported

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
 import react from 'react';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch.js';
+import { TodoList } from './TodoList.js';
+import { TodoItem } from './TodoItem.js';
