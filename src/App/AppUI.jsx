05d8d58d23636581c0a7d59d6d21d7f5e19a608e
Christian <c.espinoza.code@gmail.com>
2023-03-21 13:49:38 -0500

    TodoSearch component imported

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -1,3 +1,4 @@
 import React from 'react';
 import { TodoContext } from '../context/index.jsx';
 import { TodoCounter } from '../components/TodoCounter/index.jsx';
+import { TodoSearch } from '../components/TodoSearch/index.jsx';
