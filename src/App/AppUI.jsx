4a5970caec0e7a8dd320f5d09b4637f3e5b87bbd
Christian <c.espinoza.code@gmail.com>
2023-03-22 13:50:34 -0500

    TodoItem component imported

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -3,3 +3,4 @@ import { TodoContext } from '../context/index.jsx';
 import { TodoCounter } from '../components/TodoCounter/index.jsx';
 import { TodoSearch } from '../components/TodoSearch/index.jsx';
 import { TodoList } from '../components/TodoList/index.jsx';
+import { TodoItem } from '../components/TodoItem/index.jsx';
