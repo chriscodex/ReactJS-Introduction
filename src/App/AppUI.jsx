0d822be2c31cecf8ca3f47857ab9a978ed15fc51
Christian <c.espinoza.code@gmail.com>
2023-03-22 13:50:05 -0500

    TodoList component imported

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -2,3 +2,4 @@ import React from 'react';
 import { TodoContext } from '../context/index.jsx';
 import { TodoCounter } from '../components/TodoCounter/index.jsx';
 import { TodoSearch } from '../components/TodoSearch/index.jsx';
+import { TodoList } from '../components/TodoList/index.jsx';
