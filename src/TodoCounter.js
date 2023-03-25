98a993d8fc96e51572eb39434d29c7d6bd82fd11
Christian <c.espinoza.code@gmail.com>
2023-03-25 08:55:07 -0500

    react imported on todoCounter component

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,16 +1,1 @@
 import React from "react";
-import './TodoCounter.css';
-
-// const estilos = {
-//     color: 'red',
-//     // background-color
-//     backgroundColor: 'yellow'
-// }
-
-function TodoCounter(){
-    return(
-        <h2 className="TodoCounter"> Has completado 2 tareas de 3</h2>
-    )
-}
-
-export {TodoCounter};
