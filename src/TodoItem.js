84367448a6e9b62f79a52248290009f04fe5af81
Christian <c.espinoza.code@gmail.com>
2023-03-26 09:01:30 -0500

    text conditional added

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -6,6 +6,7 @@ function TodoItem(props){
         <li className="TodoItem">
             <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>√</span>
             {/* Condicional, si el TodoItem recibe una props que se llama completed, entonces se agrega la clase del costado*/}
+            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>{props.text}</p>
         </li>
     );
 }
