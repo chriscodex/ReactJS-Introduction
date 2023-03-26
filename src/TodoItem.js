ceb2512e72984b475bfd2c2047ae7c818eb224bf
Christian <c.espinoza.code@gmail.com>
2023-03-26 09:00:43 -0500

    check icon span added

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,6 +4,7 @@ import './TodoItem.css';
 function TodoItem(props){
     return(
         <li className="TodoItem">
+            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>√</span>
         </li>
     );
 }
