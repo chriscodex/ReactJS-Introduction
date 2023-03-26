0783ec465b7b789724b19b7656942d797eff4145
Christian <c.espinoza.code@gmail.com>
2023-03-26 09:01:01 -0500

    comment added

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,6 +5,7 @@ function TodoItem(props){
     return(
         <li className="TodoItem">
             <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>√</span>
+            {/* Condicional, si el TodoItem recibe una props que se llama completed, entonces se agrega la clase del costado*/}
         </li>
     );
 }
