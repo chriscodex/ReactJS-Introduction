8a64d7cdbb81bfb1fa668925c1ebe92151b7966c
Christian <c.espinoza.code@gmail.com>
2023-03-27 09:02:08 -0500

    X icon added

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -7,6 +7,7 @@ function TodoItem(props){
             <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>√</span>
             {/* Condicional, si el TodoItem recibe una props que se llama completed, entonces se agrega la clase del costado*/}
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>{props.text}</p>
+            <span className="Icon Icon-delete">X</span>
         </li>
     );
 }
