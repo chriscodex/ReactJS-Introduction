219e96a4b919880a7efd014ae54de76a4f9dce19
Christian <c.espinoza.code@gmail.com>
2023-03-27 09:05:24 -0500

    react imported on TodoList component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,1 @@
 import React from "react";
-import './TodoList.css'
-
-function TodoList(props){
-    return(
-        <section>
-            <ul>
-                {props.children}
-            </ul>
-        </section>
-    );
-}
-
-export { TodoList};
