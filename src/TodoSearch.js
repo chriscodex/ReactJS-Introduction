a2ec57f716da5c3c682b3848b8c6e914bb6f745e
Christian <c.espinoza.code@gmail.com>
2023-03-27 09:09:18 -0500

    react imported on todoSearch component

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,10 +1,1 @@
 import React from 'react';
-import './TodoSearch.css';
-
-function TodoSearch() {
-  return (
-    <input className="TodoSearch" placeholder="Cebolla" />
-  );
-}
-
-export { TodoSearch };
