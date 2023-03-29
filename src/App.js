7881b6b8fa54371e86ca754d9b536a042e542a36
Christian <c.espinoza.code@gmail.com>
2023-03-29 09:33:07 -0500

    CreateTodoButtom name changed in import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch.js';
 import { TodoList } from './TodoList.js';
 import { TodoItem } from './TodoItem.js';
-import { CreateTodoButtom } from './CreateTodoButton.js';
+import { CreateTodoButtom } from './CreateTodoButtom.js';
 //import './App.css';
 
 const todos = [
