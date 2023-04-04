ee88522bc4379207ae676fd7e97f46bf463f5953
Christian <c.espinoza.code@gmail.com>
2023-04-03 23:44:58 -0500

    event interaction with ReactHooks implemented in TodoSearch component

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,12 +2,36 @@ import React from "react";
 import './TodoItem.css';
 
 function TodoItem(props){
+    const onComplete = () => {
+        alert(`Tarea ${props.text} completada`)
+    }
+
+    const onDelete = () => {
+        alert(`Tarea ${props.text} eliminada`)
+    }
+
     return(
         <li className="TodoItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>√</span>
+            <span 
+                className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+                onClick={onComplete}
+            >
+                √
+            </span>
+
             {/* Condicional, si el TodoItem recibe una props que se llama completed, entonces se agrega la clase del costado*/}
-            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>{props.text}</p>
-            <span className="Icon Icon-delete">X</span>
+            <p 
+                className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}
+            >
+                {props.text}
+            </p>
+
+            <span 
+                className="Icon Icon-delete"
+                onClick={onDelete}
+            >
+                X
+            </span>
         </li>
     );
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,9 +2,34 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
-  return (
-    <input className="TodoSearch" placeholder="Cebolla" />
-  );
+  // React hook - useState
+  // Estado inicial: '' - String vacío
+  const [searchValue, setSearchValue] = React.useState('')
+  
+  // Evento de escritura/texto en el input
+  const onSearchValueChange = (event) => {
+    const value = event.target.value
+
+    // Guardar el valor en el estado setSearchValue
+    console.log(value)
+    setSearchValue(value)
+  }
+
+  return [
+    <input
+      key="search-input"
+      className="TodoSearch" 
+      placeholder="Buscar"
+      // React Doc: El valor de value debe ser igual a 'searchValue'
+      value={searchValue}
+      onChange={onSearchValueChange}
+    />,
+    <p
+      key="search-value"
+    >
+      {searchValue}
+    </p>
+  ];
 }
 
 export { TodoSearch };
