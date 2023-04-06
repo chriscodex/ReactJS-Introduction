3a14d1313ba5217fbc97494e763cf884b6d053f3
Christian <c.espinoza.code@gmail.com>
2023-04-06 12:12:42 -0500

    const variables from useLocalStorage deconstrured

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -4,5 +4,10 @@ import { useLocalStorage } from './useLocalStorage';
 // Creación del contexto
 const TodoContext = React.createContext()
 
+function TodoProvider(props) {
+  const {
+  } = useLocalStorage('TODOS_V1', []);
+
+}
 
 export { TodoContext, TodoProvider }
