ce7ae24cc385b4dc7cce3dabe86629118c49e19d
Christian <c.espinoza.code@gmail.com>
2023-04-11 14:00:00 -0500

    root created in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,3 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/index.jsx';
 import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
