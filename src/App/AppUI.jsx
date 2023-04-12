5048863a859f12ceb44274685641538cc1236949
Christian <c.espinoza.code@gmail.com>
2023-04-12 10:27:23 -0500

    CreateTodoButtom component imported to AppUI.jsx

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -3,3 +3,4 @@ import { TodoCounter } from '../components/TodoCounter/index.jsx';
 import { TodoSearch } from '../components/TodoSearch/index.jsx';
 import { TodoList } from '../components/TodoList/index.jsx';
 import { TodoItem } from '../components/TodoItem/index.jsx';
+import { CreateTodoButtom } from '../components/CreateTodoButtom/index.jsx';
