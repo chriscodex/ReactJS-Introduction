a62bb5f85d70b255dda9e5f5420b3388c4e4b5a7
Christian <c.espinoza.code@gmail.com>
2023-04-12 10:27:04 -0500

    todoItem component imported to AppUI.jsx

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -2,3 +2,4 @@ import React from 'react';
 import { TodoCounter } from '../components/TodoCounter/index.jsx';
 import { TodoSearch } from '../components/TodoSearch/index.jsx';
 import { TodoList } from '../components/TodoList/index.jsx';
+import { TodoItem } from '../components/TodoItem/index.jsx';
