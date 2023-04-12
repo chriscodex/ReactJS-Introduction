19b33a26bccf01aa6499df93cc231f047834bcc1
Christian <c.espinoza.code@gmail.com>
2023-04-12 10:29:06 -0500

    error received as component property

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -5,4 +5,10 @@ import { TodoList } from '../components/TodoList/index.jsx';
 import { TodoItem } from '../components/TodoItem/index.jsx';
 import { CreateTodoButtom } from '../components/CreateTodoButtom/index.jsx';
 
+function AppUI({
+  error,
+}) {
+  
+}
+
 export { AppUI }
