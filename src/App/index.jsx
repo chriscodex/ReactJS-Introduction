3f8c9cac1bbb1c13a3792079d9f2f7d57189af24
Christian <c.espinoza.code@gmail.com>
2023-04-13 23:38:07 -0500

    default todos array added

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -3,3 +3,9 @@ import { TodoProvider } from '../context/index.jsx';
 import { AppUI } from './AppUI.jsx';
 
 //import './App.css';
+
+// const defaultTodos = [
+//   { text: 'Tarea 1', completed: false },
+//   { text: 'Tareas 2', completed: true },
+//   { text: 'Tarea 3', completed: false },
+// ];
