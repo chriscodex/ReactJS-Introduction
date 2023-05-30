e981e752c5413d6a6b7de36b3f18c8d777e9a284
Christian <c.espinoza.code@gmail.com>
2023-05-30 18:51:36 -0500

    App render refatored

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/index.jsx';
 import './index.css';
+import App from './App';
 
-// Creación de la raíz de renderizado 'root'
-// Una raíz de renderizado es un objeto que React utiliza para vincular la aplicación de React a un nodo del DOM en la página web.
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
