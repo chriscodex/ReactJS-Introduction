3ca95b545fea7f2836e8a9617a923741473a0a4f
Christian <c.espinoza.code@gmail.com>
2023-05-31 19:07:16 -0500

    App UI refactorized using context instead of properties

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -1,55 +1,42 @@
 import React from 'react';
-import { TodoContext } from '../context/index.jsx';
-import { TodoCounter } from '../components/TodoCounter/index.jsx';
-import { TodoSearch } from '../components/TodoSearch/index.jsx';
-import { TodoList } from '../components/TodoList/index.jsx';
-import { TodoItem } from '../components/TodoItem/index.jsx';
-import { CreateTodoButtom } from '../components/CreateTodoButtom/index.jsx';
-import { Modal } from '../components/Modal/index.jsx';
-import { TodoForm } from '../components/TodoForm/index.jsx';
+import { TodoCounter } from '../components/TodoCounter';
+import { TodoSearch } from '../components/TodoSearch';
+import { TodoList } from '../components/TodoList';
+import { TodoItem } from '../components/TodoItem';
+import { CreateTodoButtom } from '../components/CreateTodoButtom';
+import { TodosLoading } from '../components/TodosLoading';
+import { TodosError } from '../components/TodosError';
+import { EmptyTodos } from '../components/EmptyTodos';
+import { Modal } from '../components/Modal';
+import { TodoForm } from '../components/TodoForm';
+
+import { TodoContext } from '../context/TodoContext';
 
 function AppUI() {
-  const {
-    error,
-    loading,
-    searchedTodos,
-    completeTodo,
-    deleteTodo,
-    openModal,
-    setOpenModal,
-  } = React.useContext(TodoContext);
+  const { loading, error, searchedTodos, onComplete, onDelete, openModal } =
+    React.useContext(TodoContext);
 
   return (
-    <React.Fragment>
+    <>
       <TodoCounter />
       <TodoSearch />
-
       <TodoList>
-        {error && <p>Desespérate, hubo un error...</p>}
-        {loading && <p>Estamos cargando, no desesperes...</p>}
-        {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
-        
-        {searchedTodos.map(todo => (
+        {loading && <TodosLoading />}
+        {error && <TodosError />}
+        {!loading && searchedTodos.length === 0 && <EmptyTodos />}
+        {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
+            onComplete={() => onComplete(todo.text)}
+            onDelete={() => onDelete(todo.text)}
           />
         ))}
       </TodoList>
-
-      {openModal && (
-        <Modal>
-          <TodoForm />
-        </Modal>
-      )}
-
-      <CreateTodoButtom
-        setOpenModal={setOpenModal}
-      />
-    </React.Fragment>
+      <CreateTodoButtom />
+      {openModal && <Modal><TodoForm></TodoForm></Modal>}
+    </>
   );
 }
 
